416383838ca3202d46f4adad192709bac9cd0ac8
Christian Stamati <christian.stamati@gmail.com>
2024-05-29 21:23:53 +0200

    redeploy next 15

--- src/env.ts
+++ src/env.ts
@@ -1,5 +1,4 @@
 import { createEnv } from "@t3-oss/env-nextjs";
-import { z } from "zod";
 import dotenv from "dotenv";
 
 dotenv.config({
@@ -8,11 +7,7 @@ dotenv.config({
 
 // docs: https://env.t3.gg/docs/nextjs#use-your-schema
 export const env = createEnv({
-  server: {
-    /*DATABASE_URL: z.string().url(),
-    DATABASE_AUTH_TOKEN: z.string(),*/
-  },
-
+  server: {},
   // For Next.js >= 13.4.4, you only need to destructure client variables:
   client: {},
   experimental__runtimeEnv: {},
