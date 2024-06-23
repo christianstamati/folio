4174a4c0ca90a0ed02921584073604f9b8780942
christianstamati <christian.stamati@gmail.com>
2024-06-23 08:36:09 +0200

    fix build error

--- src/app/dev/project-card/page.tsx
+++ src/app/dev/project-card/page.tsx
@@ -2,8 +2,6 @@ import ProjectCard from "@/components/project-card";
 
 export default function ProjectCardDev() {
   return (
-    <div className="flex h-full items-center justify-center bg-blue-100 px-6 py-16">
-      <ProjectCard />
-    </div>
+    <div className="flex h-full items-center justify-center bg-blue-100 px-6 py-16"></div>
   );
 }
